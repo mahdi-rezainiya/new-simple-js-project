829d876064df4f916edaafd5562e81c11a172557
mahdi <mahdi.rezainiya@gmail.com>
2024-05-14 19:39:01 +0330

    new changes

--- app.js
+++ app.js
@@ -100,7 +100,6 @@ let arr2 = [5 , 84 , 54 , 23 , 64 , 91];
 
 // console.log(a === b);
 
-
 // function greet (a , b){
 //     return a * b;
 // }
@@ -117,14 +116,11 @@ let arr2 = [5 , 84 , 54 , 23 , 64 , 91];
 //     }
 // }
 
-
-
 // let i =0 ;
 // while(i < 5){
 //     console.log(i);
 //     i++
 // }
-
 
 // for(let arr of arr1){
 //     console.log(arr);
@@ -133,8 +129,6 @@ let arr2 = [5 , 84 , 54 , 23 , 64 , 91];
 // for(let arr in arr1){
 //     console.log(arr1[arr]);
 // }
-
-
 
 // for(let i = 0 ; i < 20 ; i++){
 //     if(i === 8){
@@ -151,8 +145,6 @@ let arr2 = [5 , 84 , 54 , 23 , 64 , 91];
 //     return item
 // })
 // console.log(a);
-
-
 
 
 // let filterArr1 = arr1.filter((item) => {
@@ -195,18 +187,17 @@ let arr2 = [5 , 84 , 54 , 23 , 64 , 91];
 // let message = (age >= 10) ? "great" : "no";
 // console.log(message);
 
-
 // global scope
 // function scope
 // block scope
 
-let obj1 = {
-    fname : "mahdi",
-    lname : "rezainiya",
-    age : 20 ,
-    country : "iran",
-    city : "tabriz"
-}
+// let obj1 = {
+//     fname : "mahdi",
+//     lname : "rezainiya",
+//     age : 20 ,
+//     country : "iran",
+//     city : "tabriz"
+// }
 
 // obj1.sayHello()
 
@@ -216,17 +207,13 @@ let obj1 = {
 // let {age} = obj1;
 // console.log(age);
 
-let arrayOne = ["mahdi" , "ali" , "maryam" , "nima" , "kosar" , "leila"] ;
+// let arrayOne = ["mahdi" , "ali" , "maryam" , "nima" , "kosar" , "leila"] ;
 
-
-
-
-let {fname , age , country , ...rest} = obj1;
+// let {fname , age , country , ...rest} = obj1;
 // console.log(fname , age , country);
 // console.log(rest);
 
-
-let [one , two , three , ...restone] = arrayOne ;
+// let [one , two , three , ...restone] = arrayOne ;
 
 // console.log(one);
 // console.log(two);
@@ -234,17 +221,193 @@ let [one , two , three , ...restone] = arrayOne ;
 // console.log(restone);
 
 
-delete obj1.lname;
+// delete obj1.lname;
 
-let obj2 = {...obj1 , language : "turkish" , nationality : "persian"};
-let arrayTwo = [...arrayOne , "micheal" , "john"];
+// let obj2 = {...obj1 , language : "turkish" , nationality : "persian"};
+// let arrayTwo = [...arrayOne , "micheal" , "john"];
 // console.log(arrayTwo);
 // console.log(obj2);
 
 
-
 // const neme = "ali";
 // console.log(neme.split("").reverse().join(""));
+
+
+// // ایجاد یک Array از اعداد
+// const numbers = [1, 2, 3, 4, 5];
+
+// // حلقه For Loop برای چاپ اعداد Array
+// for (let i = 0; i < numbers.length; i++) {
+//     // TODO: 
+//     console.log(numbers[i]);
+// }
+
+
+
+// const userInput = prompt("Please enter a positive integer:");
+
+// const number = parseInt(userInput);
+
+// let currentNumber = 0;
+
+// while (currentNumber < number) {
+//     // TODO 
+//     if(currentNumber <= number){
+//         console.log(currentNumber);
+//     }
+//     currentNumber++;
+// }
+
+
+// const strings = ["apple", "banana", "cherry"];
+
+// for (let str of strings) {
+//     // TODO     
+//     for(let char of str){
+//         console.log(char);
+//     }
+// }
+
+
+// let fname = "mahdi";
+// console.log(name.trim());
+// console.log(fname.slice(0 , 2));
+// let num = "2442";
+// let pars = parseInt(num);
+// console.log(typeof pars);
+// let numbers = [4 , 7 , 8  , 47 , 87 , 89 , 105 , 35];
+// console.log(numbers.reverse());
+// numbers.forEach((item) => {
+//     console.log(item);
+// })
+// let numExport = numbers.map((item) => {
+//     // return `${item + "2"}`;
+//     return typeof item;
+// })
+// console.log(numExport);
+
+// let filterNum = numbers.filter((item) => {
+//     return item === 4 ;
+// })
+
+// console.log(filterNum);
+
+// let findNum = numbers.find((item) => {
+//     return item > 20;
+// })
+
+// console.log(findNum);
+
+// let reduceNum = numbers.reduce((sum , cValue) => {
+//     return sum += cValue;
+// })
+
+
+// let someNum = numbers.every((item) => {
+//     return item > 3  ;
+// })
+
+// console.log(someNum);
+// console.log(reduceNum);
+
+// setInterval(() => {
+//     console.log("hello world");
+// }, 2000);
+
+// console.log(Math.min(...numbers));;
+
+// const [first , second , ...rest] = numbers ;
+// console.log(first);
+// console.log(second);
+// console.log(rest);
+
+// let obj1 = {
+//     fname : "mahdi",
+//     lname : "rezainiya",
+//     age : 22 ,
+//     country : "Iran",
+//     city : "tabriz"
+// }
+// let obj2 = {
+//     fname : "ali",
+//     lname : "fateh",
+//     age : 23 ,
+//     country : "Iran",
+//     city : "shiraz"
+// }
+
+
+// const {age , fname , ...rest} = obj1;
+
+// console.log(age , fname , rest);
+
+// let obj = {...obj1 , ...obj2};
+// console.log(obj);
+
+
+
+
+// const numbers = [1 , 2 , 3 , 4 , 5];
+
+
+// for(let i = 0 ; i < numbers.length ; i++){
+//     console.log(numbers[i]);
+// }
+
+// const userInput = prompt("please enter a positive integer :");
+// const number = parseInt(userInput);
+// let currentNumber = 0 ;
+// while(currentNumber < number){
+//     if(currentNumber % 2 === 0){
+//         console.log(currentNumber);
+//     }
+//     currentNumber ++ ;
+// }
+
+// const strings = ["apple" , "orange" , "banana" , "cherry"];
+// for(let string of strings){
+//     for(let str of string){
+//         console.log(str);
+//     }
+// }
+
+// const numbers = [1 , 3 , 5 , 7 , 9 , 11]; 
+// for(let number of numbers){
+//     console.log(number);
+//     if(number > 5){
+//         break;
+//     }
+// }
+
+// const numbers = [1 , 2 , 3 , 4 , 5];
+// const powerTwo = (num) => {return num**2};
+// for(let number of numbers){
+//     console.log(powerTwo(number));
+// }
+
+const names = ["      a  k  o     " , "      fate     meh" , "     n  ima" , "  mahdi  " , "mayam    "];
+// const names = "fatemeh";
+
+// console.log(names.split("").reverse().join(""));
+
+// function arrayTrim(array){
+//     const newArray = array.map((arr) => {
+//         return arr.trim().replaceAll(" " , "")
+//     })
+//     return newArray;
+// }
+// console.log(arrayTrim(names));
+
+// let item = 0 ;
+// const timer = setInterval(()=>{
+//     console.log(item++);
+//     if(item ===10){
+//         clearInterval(timer)
+//     }
+// } , 1000)
+
+
+
 
 
 
